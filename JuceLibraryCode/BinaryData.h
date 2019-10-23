9252649c45ad6530acad1340e1cc44c1bd808878
Taylor Govers <tgovers1998@gmail.com>
2019-10-24 06:10:28 +1300

    First Commit of Initial Demon Release

---
+++ JuceLibraryCode/BinaryData.h
@@ -0,0 +1,105 @@
+/* =========================================================================================
+
+   This is an auto-generated file: Any edits you make may be overwritten!
+
+*/
+
+#pragma once
+
+namespace BinaryData
+{
+    extern const char*   DemonKnob_png;
+    const int            DemonKnob_pngSize = 326489;
+
+    extern const char*   A26_png;
+    const int            A26_pngSize = 299777;
+
+    extern const char*   A25_png;
+    const int            A25_pngSize = 298883;
+
+    extern const char*   A24_png;
+    const int            A24_pngSize = 296267;
+
+    extern const char*   A23_png;
+    const int            A23_pngSize = 299784;
+
+    extern const char*   A21_png;
+    const int            A21_pngSize = 296214;
+
+    extern const char*   A20_png;
+    const int            A20_pngSize = 299979;
+
+    extern const char*   A19_png;
+    const int            A19_pngSize = 299767;
+
+    extern const char*   A18_png;
+    const int            A18_pngSize = 296233;
+
+    extern const char*   A17_png;
+    const int            A17_pngSize = 299940;
+
+    extern const char*   A16_png;
+    const int            A16_pngSize = 298937;
+
+    extern const char*   A15_png;
+    const int            A15_pngSize = 297072;
+
+    extern const char*   A14_png;
+    const int            A14_pngSize = 301798;
+
+    extern const char*   A13_png;
+    const int            A13_pngSize = 301209;
+
+    extern const char*   A12_png;
+    const int            A12_pngSize = 296501;
+
+    extern const char*   A11_png;
+    const int            A11_pngSize = 299906;
+
+    extern const char*   A10_png;
+    const int            A10_pngSize = 299155;
+
+    extern const char*   A9_png;
+    const int            A9_pngSize = 295935;
+
+    extern const char*   A8_png;
+    const int            A8_pngSize = 300000;
+
+    extern const char*   A7_png;
+    const int            A7_pngSize = 299020;
+
+    extern const char*   A6_png;
+    const int            A6_pngSize = 296437;
+
+    extern const char*   A5_png;
+    const int            A5_pngSize = 299850;
+
+    extern const char*   A4_png;
+    const int            A4_pngSize = 299184;
+
+    extern const char*   A3_png;
+    const int            A3_pngSize = 297201;
+
+    extern const char*   A2_png;
+    const int            A2_pngSize = 298837;
+
+    extern const char*   A1_png;
+    const int            A1_pngSize = 296306;
+
+    // Number of elements in the namedResourceList and originalFileNames arrays.
+    const int namedResourceListSize = 26;
+
+    // Points to the start of a list of resource names.
+    extern const char* namedResourceList[];
+
+    // Points to the start of a list of resource filenames.
+    extern const char* originalFilenames[];
+
+    // If you provide the name of one of the binary resource variables above, this function will
+    // return the corresponding data and its size (or a null pointer if the name isn't found).
+    const char* getNamedResource (const char* resourceNameUTF8, int& dataSizeInBytes);
+
+    // If you provide the name of one of the binary resource variables above, this function will
+    // return the corresponding original, non-mangled filename (or a null pointer if the name isn't found).
+    const char* getNamedResourceOriginalFilename (const char* resourceNameUTF8);
+}
